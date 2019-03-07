fb40067852d9540d380a6d997b52b27a00c719a1
Michael Carroll <carrollm2@gmail.com>
2019-03-07 01:51:15 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,9 +2,9 @@ class TicTacToe
   # WIN_COMBINATIONS a constant
   WIN_COMBINATIONS = [[0, 1, 2],[3, 4, 5],[6, 7, 8],[0, 3, 6],[1, 4, 7],[2, 5, 8],[0, 4, 8],[2, 4, 6]]
 
-  def initialize(empty_board)
-    @board = empty_board
-  end
+  #def initialize(empty_board)
+    #@board = empty_board
+  #end
 
   def current_player(board)
     turn_number = turn_count(board) + 1
